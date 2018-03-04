4a1aff3a87cbbbc166062f233b8c47d0befb06bb
kamranazmat <azmat.kamran@gmail.com>
2018-03-04 16:18:24 +0000

    adding comment for deployed contact address

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 abi =JSON.parse('[{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"totalVotesFor","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"validCandidate","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votesReceived","outputs":[{"name":"","type":"uint8"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"x","type":"bytes32"}],"name":"bytes32ToString","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidateList","outputs":[{"name":"","type":"bytes32"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"voteForCandidate","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"contractOwner","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"inputs":[{"name":"candidateNames","type":"bytes32[]"}],"payable":false,"type":"constructor"}]')
 
 VotingContract = web3.eth.contract(abi);
-contractInstance = VotingContract.at('0x1fc3a1e846d4e09cf6f20863235a1513edfdb233');
+
+// update deployedContractAddress after deploying
+let deployedContractAddress = '0x78686e6050328bbb8e317465e32c22979028c8bd'
+contractInstance = VotingContract.at(deployedContractAddress);
 
 candidates = {"Rama": "candidate-1", "Nick": "candidate-2", "Jose": "candidate-3"}
 
